204006fa66f9248efab13f908bb2d370078b3408
Sourav Das <souravdasdip65@gmail.com>
2023-11-21 11:25:57 +0600

    filter is done

--- src/app/properties/page.tsx
+++ src/app/properties/page.tsx
@@ -3,8 +3,24 @@ import Search from "@/features/Home/components/Search";
 import { Box, SimpleGrid } from "@chakra-ui/react";
 import properties from "../../features/data/properties.json";
 
-const Properties = ({ searchParams }: { searchParams: any }) => {
-  const temp_properties = properties["hits"];
+const Properties = ({
+  searchParams,
+}: {
+  searchParams: {
+    startDate: string;
+    endDate: string;
+    room: string;
+    bath: string;
+  };
+}) => {
+  const { startDate, endDate, room, bath } = searchParams;
+  let search__room = parseInt(room) as number;
+  let search__bath = parseInt(bath) as number;
+
+  const temp_properties = properties["hits"].filter(
+    (property) =>
+      property.rooms <= search__room && property.baths <= search__bath
+  );
 
   return (
     <Box backgroundColor="#f7f8f9" padding="3rem">
